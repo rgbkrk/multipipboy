167cf41c08d8593b5936fd70bfb99e9f90916152
Kyle Kelley <rgbkrk@gmail.com>
2015-12-18 14:28:07 -0600

    Clean out some overstyling.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,15 +3,9 @@ import React from 'react';
 export const DEFAULT_PLAYER_SIZE = 2;
 
 const styles = {
-  arrow: {
+  dot: {
     position: 'absolute',
     display: 'block',
-  },
-  inner: {
-    position: 'absolute',
-    display: 'block',
-    width: 2,
-    height: 2,
   },
 };
 
@@ -42,17 +36,15 @@ export default React.createClass({
   render() {
     return (
       <div style={
-             Object.assign({}, styles.arrow, {
+             Object.assign({}, styles.dot, {
                left: `${(this.props.x / this.props.mapSize) * 100 }%`,
                top: `${(this.props.y / this.props.mapSize) * 100 }%`,
                transform: `rotate(${this.props.orientation}deg)`,
+               width: this.props.size,
+               height: this.props.size,
+               backgroundColor: this.props.color,
              })}
            onMouseOver={this.displayStats}>
-        <div style={Object.assign({}, styles.inner, {
-          backgroundColor: this.props.color,
-          width: this.props.size,
-          height: this.props.size,
-        })}/>
       </div>
     );
   },
--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -10,8 +10,8 @@ const styles = {
     width: '100%',
   },
   worldMapImage: {
-    height: '100%',
-    width: '100%',
+    /* height: '100%',
+    width: '100%', */
     zIndex: -1,
   },
 };
@@ -28,8 +28,13 @@ export const WorldMap = React.createClass({
           alt='World Map'
         />
         <Player color='#00ff00'
-                x={2000}
-                y={2000}
+                x={970}
+                y={170}
+                mapSize={2048}
+                />
+        <Player color='#ff0000'
+                x={960}
+                y={173}
                 mapSize={2048}
                 />
       </div>
