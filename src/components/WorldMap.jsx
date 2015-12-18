ce65623c70e74cba026521f0f0b681906cec268a
Kyle Kelley <rgbkrk@gmail.com>
2015-12-18 11:24:36 -0600

    Initial React structure.

---
+++ src/components/WorldMap.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const WorldMap = React.createClass({
+  displayName: 'WorldMap',
+  render() {
+    return (
+      <div>
+        <p>Mappy woo!</p>
+      </div>
+    );
+  },
+});
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { WorldMap } from './components/WorldMap';
+
+ReactDOM.render(
+  <WorldMap/>, document.querySelector('#app')
+)
