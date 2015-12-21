e1c543e9ca65ef92b54c390cfc04b29fa3847ca4
Kyle Kelley <rgbkrk@gmail.com>
2015-12-20 21:31:08 -0600

    Bring in the indentation and bring back try-catch

--- src/index.jsx
+++ src/index.jsx
@@ -8,25 +8,30 @@ Rx.DOM = DOM;
 import { withStore } from 'fluorine-lib';
 import { WorldMap } from './components/WorldMap';
 
-const opener = () => Rx.Observer.create((ev) => { console.log('Opening ', ev); });
-const events = Rx.DOM.fromEventSource('http://127.0.0.1:8090', opener())
-                     .map(data => {
-                       const potential = JSON.parse(data);
-                       if (Array.isArray(potential)) {
-                         return potential;
-                       }
-                       throw new Error('Player data not an array');
-                     })
-                     .retryWhen((attempts) => {
-                       return Rx.Observable.range(1, 32)
-                                .zip(attempts, i => i)
-                                .flatMap(i => {
-                                  return Rx.Observable.timer(Math.pow(2, i) * 1000);
-                                });
-                     })
-                     .filter(x => x !== [])
-                     .throttle(17) // Little bit under 60fps
-                     .distinctUntilChanged();
+const events = Rx.DOM
+  .fromEventSource('http://127.0.0.1:8090')
+  .map(data => {
+    try {
+      const potential = JSON.parse(data);
+      if (Array.isArray(potential)) {
+        return potential;
+      }
+    }
+    catch(e) {
+      console.error('Malformed data from server');
+    }
+    return [];
+  })
+  .retryWhen((attempts) => {
+    return Rx.Observable.range(1, 32)
+      .zip(attempts, i => i)
+      .flatMap(i => {
+        return Rx.Observable.timer(Math.pow(2, i) * 1000);
+      });
+  })
+  .filter(x => x !== [])
+  .throttle(17) // Little bit under 60fps
+  .distinctUntilChanged();
 
 const WorldMapContainer = withStore(events, 'players')(WorldMap);
 
