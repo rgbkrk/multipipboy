3d15ae1499843c7c689916e050a6183a0c926a12
Kyle Kelley <rgbkrk@gmail.com>
2015-12-21 16:24:35 -0600

    Switch to websockets via socket.io

--- src/connect.js
+++ src/connect.js
@@ -1,41 +1,39 @@
 import Rx from 'rx';
-import { DOM } from 'rx-dom';
-Rx.DOM = DOM;
+
+const io = require('socket.io-client');
 
 /**
  * Creates an observable connected to `url` with multiplayer data from that
  * endpoint if available. Will perform exponential back-off if the connection
  * goes out.
- * @param {string} url - URL of an EventSource stream
+ * @param {string} url - URL of socket.io websocket
  * @return {Observable} - stream of multiplayer data
  */
 export default function connect(url) {
-  return Rx.DOM
-    .fromEventSource(url)
-    // Parse the data if possible
-    .map(data => {
-      try {
-        const potential = JSON.parse(data);
-        if (Array.isArray(potential)) {
-          return potential;
-        }
-      }
-      catch(e) {
-        console.error('Malformed data from server');
-      }
-      return [];
-    })
-    // Exponential back-off
-    .retryWhen((attempts) => {
-      return Rx.Observable.range(1, 32) // ~100 days in total is fine for everyone
-        .zip(attempts, i => i)
-        .flatMap(i => {
-          return Rx.Observable.timer(Math.pow(2, i) * 1000);
-        });
-    })
-    // Ignore updates with nothing as well as our handled errors above
-    .filter(x => x !== [])
-    // Assume we want to only propagate data at ~60fps
-    .throttle(16)
-    .distinctUntilChanged();
+  const socketStream = Rx.Observable.create((observer) => {
+    const socket = io(url);
+    socket.on('connect', () => {
+    });
+
+    socket.on('error', (err) => {
+      console.error('my error', err);
+      observer.onError(err);
+    });
+
+    socket.on('mappy:data', (data) => {
+      observer.onNext(data);
+    });
+  });
+  return socketStream.retryWhen((attempts) => {
+    return Rx.Observable.range(0, 32) // ~100 days in total is fine for everyone
+      .zip(attempts, i => i)
+      .flatMap(i => {
+        return Rx.Observable.timer(Math.pow(2, i) * 1000);
+      });
+  })
+  // Ignore updates with nothing as well as our handled errors above
+  .filter(x => x !== [])
+  // Assume we want to only propagate data at ~60fps
+  .throttle(16)
+  .distinctUntilChanged();
 }
