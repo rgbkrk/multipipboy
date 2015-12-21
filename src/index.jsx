ab23ca4dff97abed62511a69d7f7049b7ea45c16
Kyle Kelley <rgbkrk@gmail.com>
2015-12-20 21:24:41 -0600

    Exponential backoff.

--- src/index.jsx
+++ src/index.jsx
@@ -11,17 +11,18 @@ import { WorldMap } from './components/WorldMap';
 const opener = () => Rx.Observer.create((ev) => { console.log('Opening ', ev); });
 const events = Rx.DOM.fromEventSource('http://127.0.0.1:8090', opener())
                      .map(data => {
-                       try {
-                         const potential = JSON.parse(data);
-                         if (Array.isArray(potential)) {
-                           return potential;
-                         }
-                         console.error('Player data not an array');
+                       const potential = JSON.parse(data);
+                       if (Array.isArray(potential)) {
+                         return potential;
                        }
-                       catch (e) {
-                         console.error(e);
-                       }
-                       return [];
+                       throw new Error('Player data not an array');
+                     })
+                     .retryWhen((attempts) => {
+                       return Rx.Observable.range(1, 32)
+                                .zip(attempts, i => i)
+                                .flatMap(i => {
+                                  return Rx.Observable.timer(Math.pow(2, i) * 1000);
+                                });
                      })
                      .filter(x => x !== [])
                      .throttle(17) // Little bit under 60fps
