877aa8c2a33752e94c6c0b93252b538af6a75946
Kyle Kelley <rgbkrk@gmail.com>
2015-12-23 14:02:51 -0600

    Fix tests for namespace change, export generic idx

--- src/player-model.js
+++ src/player-model.js
@@ -3,6 +3,10 @@ import { List, Map } from 'immutable';
 export const DEFAULT_WIDTH = 2048;
 export const DEFAULT_HEIGHT = 2048;
 
+export function index(x, y, width = DEFAULT_WIDTH) {
+  return width * y + x;
+}
+
 export class PlayerModel {
     constructor(width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT) {
       this.width = width;
@@ -20,7 +24,7 @@ export class PlayerModel {
     }
 
     index(x, y) {
-      return this.width * y + x;
+      return index(x, y, this.width);
     }
 
     boundsCheck(x, y) {
