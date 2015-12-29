24ede2aefe8ec3c4a7a8fc89019f469c386acee9
Kyle Kelley <rgbkrk@gmail.com>
2015-12-28 22:39:58 -0600

    More cleanup of the WorldMapStore

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import connect from './connect';
 import { WorldMapStore } from './player-store';
+import { PlayerModel } from './player-model';
+
+const batchEvents = connect();
+const ps = new PlayerModel();
+
+const playerData = batchEvents.map((playerbatch) => {
+  playerbatch.forEach(player => {
+    ps.set(player.id, player);
+  });
+  return {
+    players: ps.players,
+    playerGrid: ps.playerGrid,
+  };
+});
 
 ReactDOM.render(
-  <WorldMapStore />, document.querySelector('#app')
+  <WorldMapStore batchEvents={playerData} />, document.querySelector('#app')
 );
